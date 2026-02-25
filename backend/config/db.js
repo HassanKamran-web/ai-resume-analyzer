5279b0b07a4c40578e53893ad7edefdbb1eec8e1
Hassan <hassankamran508@gmail.com>
2026-02-26 04:17:08 +0500

    fix db logic

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+            .then(() => console.log("MongoDB connected successfully"))
+            .catch(err => {
+                console.error("MongoDB connection error:", err);
+            });
     } catch (error) {
         process.exit(1);
     }
